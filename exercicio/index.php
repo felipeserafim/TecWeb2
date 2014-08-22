40d9e33852c7ff65c0de0aaf1e2508b03cb5ba2b
31442897 <31442897@087883-P33S105.macklabs.br>
2014-08-22 08:48:36 -0300

    Exercicio 1

--- exercicio/index.php
+++ exercicio/index.php
@@ -6,7 +6,38 @@
     </head>
     <body>
         <?php
-        // put your code here
+        
+        $P1=3;
+        $M1=1;
+        $M2=1;
+        $Proj1=0.5;
+        $Proj2=0.5;
+        $Proj3=0.5;
+        $Proj4=0.5;
+        $TrabF=1;
+        $Proc=2;
+        $PF=10;
+        $MF;
+       $MI = (P1 + M1 + M2 + Proj1 + Proj2 + Proj3 +Proj4 +TrabF + Proc)/100;
+       
+               if ($MI >= 7.5){
+                   $MF=MI;
+                   echo "Aprovado sem Prova Final,Parabéns!!! Boas férias"
+               }
+       else{
+           $MF=MI+PF/100;
+       }
+        
+       if ($MF >= 5.0){
+      echo “Aprovado com Prova Final, Agora já pode descansar tranquilo.”
+       }
+       else {
+           echo "Reprovado, Não foi dessa vez. Ano que vem tem mais TWII."
+       }
+       echo $MI;
+       echo $MF;
+       
+       
         ?>
     </body>
 </html>
